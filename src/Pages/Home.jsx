602180b766f0b10a92bac058eb37d0ac7d127e80
DevMousavi <mousavii.dev@gmail.com>
2024-05-12 10:52:16 +0330

    Created Slider In "HomePage".

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,91 +1,34 @@
 import React from "react";
 import Header from "../LayOut/Header.jsx";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 const Home = () => {
     return (
         <>
             <Header />
-            <main className="bg-blue-400 container mt-6">
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <h1>Home</h1>
+            <main className=" container mt-6">
+                <div className="w-full">
+                    <div className="w-[70%]">
+                        <Swiper spaceBetween={50} slidesPerView={1}>
+                            <SwiperSlide>
+                                <img
+                                    src="./../src/assets/Slider/3.jpg"
+                                    alt="baner_1.jpg"
+                                    className="w-full rounded-2xl"
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img
+                                    src="./../src/assets/Slider/4.jpg"
+                                    alt="baner_1.jpg"
+                                    className="w-full rounded-2xl"
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
+                </div>
             </main>
         </>
     );
