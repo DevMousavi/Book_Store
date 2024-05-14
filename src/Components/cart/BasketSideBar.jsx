4e72d5ff926cbd8251c3e8528452b5b2bbb35fc4
Masoomeh Aslani <masiw.rad7@gmail.com>
2024-05-14 23:17:03 +0330

    basket sidebar added.

---
+++ src/Components/cart/BasketSideBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+
+function BasketSideBar() {
+  return (
+    <div>
+      <div className="bg-white pt-24 px-8 pb-8 relative min-w-72 min-h-80 rounded-3xl mb-4">
+        <div className="absolute top-0 right-[123px]">
+          <img
+            src="./../src/assets/shop.png"
+            alt="cart-logo"
+            width="50"
+            height="50"
+          />
+        </div>
+        <div className="flex justify-evenly mt-2">
+          <p>تعداد کالاها : </p>
+          <span> 3</span>
+        </div>
+        <div className="flex justify-evenly border-b-2 border-dashed border-slate-400 pb-7 mt-4">
+          <p>جمع مبلغ کالاها : </p>
+          <span> 220000 تومان</span>
+        </div>
+        <div className="flex justify-evenly mt-7">
+          <p>وضعیت پرداخت : </p>
+          <span>پرداخت نشده </span>
+        </div>
+      </div>
+      <div className="bg-primaryGreen text-white w-full rounded-xl h-14 flex items-center justify-center font-bold my-4">
+        <button >
+          پرداخت
+        </button>
+        <IoIosArrowBack />
+      </div>
+    </div>
+  );
+}
+
+export default BasketSideBar;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../LayOut/Header.jsx";
+import BasketCard from "../Components/cart/BasketCard.jsx";
+import BasketSideBar from "../Components/cart/BasketSideBar.jsx";
 
 const Cart = () => {
   return (
     <>
       <Header />
-      <h1>Cart</h1>
+      <div className="flex justify-between container my-20">
+        <BasketCard />
+        <BasketSideBar />
+      </div>
     </>
   );
 };
