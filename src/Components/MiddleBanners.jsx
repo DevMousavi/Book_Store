8633b8218ab9983e3e9674fbecf717669dc5d9ce
Masoomeh Aslani <masiw.rad7@gmail.com>
2024-05-14 15:16:34 +0330

    body of home page completed

---
+++ src/Components/MiddleBanners.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MiddleBanners() {
+  return (
+    <div className='flex flex-row-reverse justify-around mb-6'>
+        <img src="./../src/assets/middle-offers/1.jpg" alt="offer-banner" className='h-36 rounded-3xl cursor-pointer'/>
+        <img src="./../src/assets/middle-offers/2.jpg" alt="offer-banner" className='h-36 rounded-3xl cursor-pointer'/>
+        <img src="./../src/assets/middle-offers/3.jpg" alt="offer-banner" className='h-36 rounded-3xl cursor-pointer'/>
+        <img src="./../src/assets/middle-offers/4.jpg" alt="offer-banner" className='h-36 rounded-3xl cursor-pointer'/>
+    </div>
+  )
+}
+
+export default MiddleBanners
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,89 +11,109 @@ import "swiper/css";
 import PartnerBrandsBox from "../Components/PartnerBrandsBox.jsx";
 import ContainerSimple from "../Components/ContainerSimple.jsx";
 import ColoredContainer from "../Components/ColoredContainer.jsx";
+import MiddleBanners from "../Components/MiddleBanners.jsx";
 
 const Home = () => {
-    return (
-        <>
-            <Header />
-            <main className=" container mt-6">
+  return (
+    <>
+      <Header />
+      <main className=" container mt-6">
+        <img
+          src="./../src/assets/baner_1.png"
+          alt="baner_1.png"
+          className="my-2 "
+        />
+        <div className="w-full flex items-center justify-between">
+          <div className="w-[70%] cursor-pointer cardOfferShadow rounded-2xl">
+            <Swiper spaceBetween={50} slidesPerView={1}>
+              <SwiperSlide>
                 <img
-                    src="./../src/assets/baner_1.png"
-                    alt="baner_1.png"
-                    className="my-2 "
+                  src="./../src/assets/Slider/3.jpg"
+                  alt="baner_1.jpg"
+                  className="w-full rounded-2xl"
                 />
-                <div className="w-full flex items-center justify-between">
-                    <div className="w-[70%] cursor-pointer cardOfferShadow rounded-2xl">
-                        <Swiper spaceBetween={50} slidesPerView={1}>
-                            <SwiperSlide>
-                                <img
-                                    src="./../src/assets/Slider/3.jpg"
-                                    alt="baner_1.jpg"
-                                    className="w-full rounded-2xl"
-                                />
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <img
-                                    src="./../src/assets/Slider/4.jpg"
-                                    alt="baner_1.jpg"
-                                    className="w-full rounded-2xl"
-                                />
-                            </SwiperSlide>
-                        </Swiper>
-                    </div>
-                    <div className="w-[25%] h-[350px] rounded-2xl ">
-                        <img
-                            src="./../src/assets/baner_2.png"
-                            alt="baner_2.png"
-                            className="w-full h-full rounded-2xl"
-                        />
-                    </div>
-                </div>
-                <DiscountedProducts />
-                <h2 className="text-3xl text-primaryGreen mt-14">
-                    همکاری ها ...
-                </h2>
-                <PartnerBrandsBox />
-                <ContainerSimple
-                    url="educational?_page=1&_limit=5"
-                    link="/educational"
-                    title="کمک درسی ..."
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src="./../src/assets/Slider/4.jpg"
+                  alt="baner_1.jpg"
+                  className="w-full rounded-2xl"
                 />
-                <ColoredContainer
-                    url="/adults?_page=1&_limit=4"
-                    title="عمومی بزرگ سالان..."
-                    link="/adults_products"
-                    bg_color="bg-lightBlue"
-                    img_title="adults"
-                />
-                <ContainerSimple
-                    url="english_products?_page=1&_limit=5"
-                    link="/english_products"
-                    title="کتاب های انگلیسی..."
-                />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div className="w-[25%] h-[350px] rounded-2xl ">
+            <img
+              src="./../src/assets/baner_2.png"
+              alt="baner_2.png"
+              className="w-full h-full rounded-2xl"
+            />
+          </div>
+        </div>
+        <DiscountedProducts />
+        <h2 className="text-3xl text-primaryGreen mt-14">همکاری ها ...</h2>
+        <PartnerBrandsBox />
+        <ContainerSimple
+          url="educational?_page=1&_limit=5"
+          link="/educational"
+          title="کمک درسی"
+        />
+        <ColoredContainer
+          url="/adults?_page=1&_limit=4"
+          title="عمومی بزرگسال"
+          link="/adults_products"
+          bg_color="bg-lightBlue"
+          img_title="adults"
+        />
+        <ContainerSimple
+          url="english_products?_page=1&_limit=5"
+          link="/english_products"
+          title="کتاب های انگلیسی"
+        />
 
-                <div className="flex flex-col gap-1 mb-9">
-                    <img
-                        src="./../src/assets/b-1.png"
-                        alt="b-1.png"
-                        className="w-full cardOfferShadow rounded-xl"
-                    />
-                    <div className="flex flex-row w-full justify-between items-center">
-                        <img
-                            src="./../src/assets/b-2.png"
-                            alt="b-2.png"
-                            className="rounded-2xl cardOfferShadow"
-                        />
-                        <img
-                            src="./../src/assets/b-3.png"
-                            alt="b-3.png"
-                            className="rounded-2xl cardOfferShadow"
-                        />
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+        <div className="flex flex-col gap-1 mb-9">
+          <img
+            src="./../src/assets/b-1.png"
+            alt="b-1.png"
+            className="w-full cardOfferShadow rounded-xl"
+          />
+          <div className="flex flex-row w-full justify-between items-center">
+            <img
+              src="./../src/assets/b-2.png"
+              alt="b-2.png"
+              className="rounded-2xl cardOfferShadow"
+            />
+            <img
+              src="./../src/assets/b-3.png"
+              alt="b-3.png"
+              className="rounded-2xl cardOfferShadow"
+            />
+          </div>
+        </div>
+        <ColoredContainer
+          url="/educational?_page=1&_limit=4"
+          title="آموزشی"
+          link="/educational"
+          bg_color="bg-darkBlue"
+          img_title="educational"
+        />
+        <ContainerSimple
+          url="kids_and_teens?_page=1&_limit=5"
+          link="/kids_and_teens"
+          title="کتاب کودک و نوجوان"
+        />
+        <MiddleBanners/>
+        <ColoredContainer
+          url="/stationery?_page=1&_limit=4"
+          title="لوازم التحریر"
+          link="/best_sellers"
+          bg_color="bg-blue"
+          img_title="stationery"
+        />
+        <img src="./../src/assets/last-banner.jpg" alt="last-banner" className="rounded-3xl cursor-pointer my-6" />
+      </main>
+    </>
+  );
 };
 
 export default Home;
