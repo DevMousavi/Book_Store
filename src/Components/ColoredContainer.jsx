dc341f61af9811b82dd30f0f543c37b4ff232331
DevMousavi <mousavii.dev@gmail.com>
2024-05-14 16:17:00 +0330

    Chenge Style title Container

--- src/Components/ColoredContainer.jsx
+++ src/Components/ColoredContainer.jsx
@@ -8,11 +8,9 @@ export const ColoredContainer = ({ url, title, link, bg_color, img_title }) => {
     const { data, isLoading } = useFetchData(url);
 
     return (
-        <div className="mb-20 relative ">
-            <div className="flex justify-end items-center">
-                <p className="font-bold text-base z-40 absolute top-16 right-[120px]">
-                    {title}
-                </p>
+        <div className="mb-20  ">
+            <div className="flex justify-between items-center">
+                <p className="font-bold text-3xl ">{title}</p>
                 <Link
                     to={link}
                     className="flex flex-row-reverse items-center gap-1 primaryTransition hover:scale-105"
@@ -31,21 +29,6 @@ export const ColoredContainer = ({ url, title, link, bg_color, img_title }) => {
                         src={`./../src/assets/colored-container/${img_title}.png`}
                         alt=""
                     />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="631"
-                        height="105"
-                        viewBox="0 0 231 75"
-                        fill="none"
-                        className="relative mb-6 left-48"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M0 0C31.5006 0.949537 50.52 17.872 56.1955 26.4544L55.986 25.8011L82.4924 58.631C99.3032 79.4521 131.038 79.4521 147.849 58.6309L174.356 25.8011L174.146 26.4544C179.822 17.872 198.844 0.949537 230.349 0H0Z"
-                            fill="#f1f5f9"
-                        ></path>
-                    </svg>
                 </div>
                 <div className="w-full flex flex-row justify-between mt-2 pl-2 pr-72">
                     {isLoading ? (
