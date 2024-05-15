df781ac752e3a9fe62c86fcfd4386b62221dc503
DevMousavi <mousavii.dev@gmail.com>
2024-05-15 14:08:20 +0330

    Pagination Was Created To Display 22 Products And Its Logic And Styling Were Well Applied.

--- src/Components/ContainerProductList.jsx
+++ src/Components/ContainerProductList.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetchData from "../Hooks/useFetchData";
 import Card from "./Card";
 
-const ContainerProductList = ({ url }) => {
+const ContainerProductList = ({ url, pageNumber }) => {
     const { data, isLoading } = useFetchData(url);
     console.log(data);
+
+    const [number1, setNumber1] = useState(1);
+    const [number2, setNumber2] = useState(22);
+
+    useEffect(() => {
+        if (pageNumber === 1) {
+            setNumber1(1);
+            setNumber2(22);
+        } else if (pageNumber === 2) {
+            setNumber1(23);
+            setNumber2(44);
+        } else if (pageNumber === 3) {
+            setNumber1(25);
+            setNumber2(66);
+        } else if (pageNumber === 4) {
+            setNumber1(67);
+            setNumber2(88);
+        } else if (pageNumber === 4) {
+            setNumber1(89);
+            setNumber2(200);
+        }
+    }, [pageNumber]);
 
     return (
         <div className="w-[77%] cardOfferShadow p-5 rounded-2xl bg-white flex flex-wrap items-center justify-between gap-x-2 gap-y-10">
@@ -12,7 +34,7 @@ const ContainerProductList = ({ url }) => {
                 <h1>Loading...</h1>
             ) : (
                 data
-                    .slice(1, 22)
+                    .slice(number1, number2)
                     .map((item) => (
                         <Card
                             key={item.id}
--- src/Pages/StationeryPage.jsx
+++ src/Pages/StationeryPage.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../LayOut/Header.jsx";
 import ContainerProductList from "../Components/ContainerProductList.jsx";
 import FilterBox from "../Components/FilterBox.jsx";
+import Pagination from "../Components/Pagination.jsx";
 
 const StationeryPage = () => {
+    const [pageNumber, setPageNumber] = useState(1);
+
     return (
         <>
             <Header />
             <main className="container w-full justify-between bg-slate-300 my-12 py-4 px-7 flex flex-row">
                 <FilterBox />
-                <ContainerProductList url="/stationery" />
+                <ContainerProductList
+                    url="/stationery"
+                    pageNumber={pageNumber}
+                />
             </main>
+            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
         </>
     );
 };
