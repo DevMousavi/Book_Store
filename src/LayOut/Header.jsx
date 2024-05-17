5805131ff37cd5f545aebd7afffa4621bc8033c7
DevMousavi <mousavii.dev@gmail.com>
2024-05-17 10:23:47 +0330

    "Header" Became Responsive For All Sizes Of Different Devices.

--- src/LayOut/Header.jsx
+++ src/LayOut/Header.jsx
@@ -7,8 +7,8 @@ import { CiSearch } from "react-icons/ci";
 import { FaStar, FaUser } from "react-icons/fa6";
 import { IoMdCart } from "react-icons/io";
 import { api } from "../server/config.js";
+import { FaBars } from "react-icons/fa";
 
-// import { useCart } from "../Context/CartContext.jsx";
 import { useCart } from "../context/CartContext.jsx";
 
 const Header = () => {
@@ -43,35 +43,43 @@ const Header = () => {
 
     return (
         <>
-            <header className="w-full pt-6  bg-white relative">
+            <header className="af:hidden md:flex w-full pt-6 bg-white relative">
                 <div className="bg-primaryGreen w-full h-16 absolute top-0 z-10"></div>
-                <div className="container flex flex-row items-center bg-white rounded-2xl px-2 relative z-20 border-b border-solid border-gray-200">
+                <div className="container flex flex-row items-center bg-white rounded-2xl px-2 md:py-0 xl:py-2 relative z-20 border-b border-solid border-gray-200">
                     <Link to="/" className="hover:scale-105 primaryTransition">
-                        <img src={LogoSRC} alt="Logo.png" />
+                        <img
+                            src={LogoSRC}
+                            alt="Logo.png"
+                            className="md:w-12 xl:w-16"
+                        />
                     </Link>
-                    <div className="w-[50%] h-12 rounded-xl flex flex-row items-center bg-slate-200 mr-7">
+                    <div className="md:w-[30%] xl:w-[50%] md:h-11 xl:h-12 rounded-xl flex flex-row items-center bg-slate-200 mr-7">
                         <CiSearch className="w-9 h-24 " />
                         <input
                             type="text"
                             placeholder="محصول خود را جست و جو کنید..."
-                            className="w-full h-11 rounded-md px-2 outline-none bg-slate-200"
+                            className="w-full h-full rounded-md px-2 outline-none bg-slate-200 md:text-xs xl:text-base"
                             onKeyDown={searchHandler}
                         />
                     </div>
-                    <div className="mr-40 flex flex-row justify-between w-[28%] items-center">
+                    <div className="md:mr-32 lg:mr-72 xl:mr-40 flex md:gap-4 xl:gap-0 flex-row justify-between w-[28%] md:h-16 xl:h-0 items-center">
                         <Link
                             to="/login"
                             className="flex items-center  flex-row gap-2 border border-black border-solid rounded-2xl px-5 py-3 hover:scale-105 primaryTransition"
                         >
                             <FaUser className="w-5 h-5" />
-                            <p>ثبت نام / ورود</p>
+                            <p className="md:text-xs xl:text-base text-nowrap">
+                                ثبت نام / ورود
+                            </p>
                         </Link>
                         <Link
                             to="/cart"
                             className="flex flex-row gap-2 items-center bg-slate-200 rounded-2xl px-5 py-3 hover:scale-105 primaryTransition "
                         >
                             <IoMdCart className="w-6 h-6" />
-                            <p> سبدخرید</p>
+                            <p className="md:text-xs xl:text-base text-nowrap">
+                                سبدخرید
+                            </p>
                             <p className="px-2 py-1 rounded-full priceShadow text-xs bg-white">
                                 {state.itemsCount}
                             </p>
@@ -113,8 +121,48 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-            <div className="w-full bg-white mt-0 headerShadow sticky top-0 z-50">
+            <div className="as:hidden md:flex w-full bg-white mt-0 headerShadow sticky top-0 z-50">
                 <CategoryBox />
+            </div>
+            {/* ===============================Responsive================================== */}
+            <header className="af:hidden as:flex as:flex-col md:hidden">
+                <div className="as:flex flex-row-reverse w-full items-center justify-between mt-4 px-4">
+                    <Link
+                        to="/login"
+                        className="flex items-center  flex-row gap-2 border border-black border-solid rounded-2xl px-2 py-2 hover:scale-105 primaryTransition"
+                    >
+                        <FaUser className="w-4 h-4" />
+                    </Link>
+                    <Link to="/" className="hover:scale-105 primaryTransition">
+                        <img
+                            src={LogoSRC}
+                            alt="Logo.png"
+                            className="w-[40%] mx-auto"
+                        />
+                    </Link>
+                    <FaBars className="w-5 h-5" />
+                </div>
+            </header>
+            <div className="af:hidden as:flex as:flex-row md:hidden bg-slate-100 py-2 headerShadow z-50 h-16 as:items-center as:justify-between mt-5 px-4 sticky top-0">
+                <div className="as:w-[80%] md:h-8 as:h-full rounded-xl flex flex-row items-center bg-slate-200 ">
+                    <CiSearch className="w-9 h-24 " />
+                    <input
+                        type="text"
+                        placeholder="محصول خود را جست و جو کنید..."
+                        className="w-full h-full rounded-md px-2 outline-none bg-slate-200 md:text-xs xl:text-base"
+                        onKeyDown={searchHandler}
+                    />
+                </div>
+                <Link
+                    to="/cart"
+                    className="flex flex-row gap-2  w-[10%] h-full items-center justify-center border border-solid  border-black rounded-2xl  hover:scale-105 primaryTransition relative"
+                >
+                    <IoMdCart className="w-6 h-6 " />
+
+                    <p className="px-1 py-0 flex items-center justify-center rounded-lg priceShadow text-sm bg-primaryPink text-white absolute bottom-0 right-0  ">
+                        {state.itemsCount}
+                    </p>
+                </Link>
             </div>
         </>
     );
